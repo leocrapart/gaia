01730581b48d8c3f7021d82fca62ce55f7703dfd
Leo Crapart <leocrapart@yahoo.fr>
2022-09-16 00:27:35 +0200

    init ui

--- src/gaia/core.clj
+++ src/gaia/core.clj
@@ -5,7 +5,6 @@
             [dotenv]))
 
 (def token      (dotenv/env "DISCORD_TOKEN"))
-token
 (def intents    #{:guilds :guild-messages})
 (def channel-id "1018526450254626841")
 
@@ -14,9 +13,6 @@ token
    "!plant" "1 tree has been planted"
    "!cut" "1 tree has been cut"
    "!leaderboard" "1 leoo#1234 (150)\n2 enzoledebilo#7896 (56)"})
-(comment
-  command-to-response
-  )
 
 (defn send-msg [message-ch channel-id content]
   (m/create-message! message-ch channel-id :content content))
@@ -33,15 +29,21 @@ token
   (try
     (loop []
       (let [[event-type event-data] (a/<!! event-ch)]
+        ; when message
         (when (and (= event-type :message-create)
                    (= (:channel-id event-data) channel-id )
                    (not (:bot (:author event-data))))
           (println (keys event-data))
           (println (str ":content " (event-data :content)))
+          ;; send message here
           (let [message (event-data :content)]
             (respond-to message-ch channel-id message)))
+
+        ; stop on pin
         (when (= :channel-pins-update event-type)
           (c/disconnect-bot! connection-ch))
+
+        ; recur
         (when-not (= :disconnect event-type)
           (recur))))
     (finally
